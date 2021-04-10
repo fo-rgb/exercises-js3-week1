048e761fcbbb3c1ff48e0bb950af471e5b0b6eca
fo-rgb <fodemakale18@gmail.com>
2021-04-10 12:49:05 +0200

    A56 exercises2 js final

--- InClass/A-ES6/exercise2.js
+++ InClass/A-ES6/exercise2.js
@@ -4,12 +4,13 @@ class Polygon {
     this.height = height;
     this.width = width;
   }
+  get area() {return this.width * this.height;}
   sayName() {
     console.log('Hi, I am a ' + this.name);
   }
 }
 
-let p = new Polygon('Polygon', 300, 400);
+let p = new Polygon(100, 300, 400);
 p.sayName();
 console.log('The area of this polygon is ' + p.area);
 
@@ -34,6 +35,36 @@ console.log('The area of this polygon is ' + r.area);
 
 //Create 2 new classes that extend from polygon: triangle and circle and log their names and areas
 
+//  Triangle
+class Triangle  extends Polygon {
+  constructor(height, width){
+    super(height, width);
+    this.name = "Triangle";
+    this.sides = 3 ;
+  }
+  get area() {return this.width * this.height;}
+  sayName(){
+  console.log('Hi I am a polygon and my name is ' + this.name + '.');
+ }
+}
+let t = new Triangle(40,60,60)
+t.sayName();
+console.log('The area of this polygon is ' + t.area)
 
 
+// Circle
 
+class Circle  extends Polygon {
+  constructor(height, width){
+    super(height, width);
+    this.name = "Circle";
+    this.sides = 4 ;
+  }
+  get area() {return this.width * this.height;}
+  sayName(){
+  console.log('Hi I am a polygon and my name is ' + this.name + '.');
+ }
+}
+let c = new Circle(330,80)
+c.sayName();
+console.log('The area of this polygon is ' + c.area)
